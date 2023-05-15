3c80c0cad73fedeb4595c05c02b3d229f483df68
Muhammad Ilham Hidayat <m.ilham.hidayat.95@gmail.com>
2023-05-15 19:00:52 +0700

    feat: implement ChargeCard for stripe

--- internal/payment/stripe.go
+++ internal/payment/stripe.go
@@ -1,8 +1,13 @@
 package payment
 
 import (
+	"context"
 	"errors"
+	"fmt"
 
+	"github.com/Rhymond/go-money"
+	"github.com/stripe/stripe-go/v74"
+	"github.com/stripe/stripe-go/v74/charge"
 	"github.com/stripe/stripe-go/v74/client"
 )
 
@@ -18,4 +23,20 @@ func NewStripeService(apiKey string) (*StripeService, error) {
 	sc.Init(apiKey, nil)
 
 	return &StripeService{stripeClient: sc}, nil
+}
+
+func (s StripeService) ChargeCard(ctx context.Context, amount money.Money, cardToken string) error {
+	params := &stripe.ChargeParams{
+		Amount:   stripe.Int64(amount.Amount()),
+		Currency: stripe.String(string(stripe.CurrencyUSD)),
+		Source: &stripe.PaymentSourceSourceParams{
+			Token: stripe.String(cardToken),
+		},
+	}
+	_, err := charge.New(params)
+	if err != nil {
+		return fmt.Errorf("failed to create a charge:%w", err)
+	}
+
+	return nil
 }
