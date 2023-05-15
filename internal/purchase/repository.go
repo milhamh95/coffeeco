6bc12d7f5ae7fc70d4575b2413194f7d065a67e2
Muhammad Ilham Hidayat <m.ilham.hidayat.95@gmail.com>
2023-05-15 07:17:58 +0700

    feat: initialize mongo repository

--- internal/purchase/repository.go
+++ internal/purchase/repository.go
@@ -1,7 +1,19 @@
 package purchase
 
-import "context"
+import (
+	"context"
+
+	"go.mongodb.org/mongo-driver/mongo"
+)
 
 type Repository interface {
 	Store(ctx context.Context, purchase Purchase) error
+}
+
+type MongoRepository struct {
+	purhcases *mongo.Collection
+}
+
+func NewMongoRepo(ctx context.Context, connectionString string) (*MongoRepository, error) {
+	return nil, nil
 }
