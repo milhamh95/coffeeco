1bdbe88f56a77387b3e8a9f4c268bd7f9891aaeb
Muhammad Ilham Hidayat <m.ilham.hidayat.95@gmail.com>
2023-05-16 09:56:58 +0700

    feat: implement store service

--- internal/store/store.go
+++ internal/store/store.go
@@ -2,6 +2,7 @@ package store
 
 import (
 	coffeeco "coffeeco/internal"
+	"context"
 
 	"github.com/google/uuid"
 )
@@ -10,4 +11,17 @@ type Store struct {
 	ID              uuid.UUID
 	Location        string
 	ProductsForSale []coffeeco.Product
+}
+
+type Service struct {
+	repo Repository
+}
+
+func (s Service) GetStoreSpecificDiscount(ctx context.Context, storeID uuid.UUID) (float32, error) {
+	dis, err := s.repo.GetStoreDiscount(ctx, storeID)
+	if err != nil {
+		return 0, err
+	}
+
+	return float32(dis), nil
 }
